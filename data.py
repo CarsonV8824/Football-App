6b5444572d525ae1941ffd84f981ed61fe79e21a
CarsonV <166647418+CarsonV8824@users.noreply.github.com>
2026-05-09 11:48:06 -0500

    done

--- data.py
+++ data.py
@@ -5,7 +5,8 @@ import pandas as pd
 def main():
 
     path = "nfl_data.csv"
-    data = pd.read_csv(path)
+    data = pd.read_csv(path, sep=";")
+    print(data.describe())
 
 if __name__ == "__main__":
     main()
