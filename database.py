19eb236bf31054ae24907d597ee3b1908b851923
CarsonV <166647418+CarsonV8824@users.noreply.github.com>
2026-05-11 21:36:59 -0500

    done

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import sqlite3
+import os
 
 class Database:
     """put data in from the csv file's into a sqlite database for ease of use."""
@@ -14,6 +15,16 @@ class Database:
         self.connection.close()
 
     @staticmethod
-    def create_tables(self):
+    def create_tables():
         with Database() as db:
-            pass
+            with open("schemas/database_tables.sql") as f:
+                data = f.read()
+
+            print(data)
+            
+            # I know this is bad. Only used to make tables.
+            db.cursor.executescript(data)
+            db.connection.commit()
+
+if __name__ == "__main__":
+    Database.create_tables()
--- scrape.py
+++ scrape.py
@@ -2,25 +2,33 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 from io import StringIO
+import time
+
+# link to data: https://fantasydata.com/nfl/fantasy-football-leaders?scope=game
 
 def get_data():
-    years = range(2001, 2026)
+    years = range(2002, 2003)
+    weeks = range(1, 5)
 
     for year in years:
-        url = f'https://fantasydata.com/nfl/fantasy-football-leaders?scope=season&sp={year}_REG&scoring=fpts_ppr&order_by=fpts_ppr&sort_dir=desc'
-        response = requests.get(url)
+        for week in weeks:
+            if week == 1:
+                continue
+            time.sleep(1)
+            url = f'https://fantasydata.com/nfl/fantasy-football-leaders?scope=game&sp=2025_REG&week_from={week - 1}&week_to={week}&scoring=fpts_ppr&order_by=fpts_ppr&sort_dir=desc'
+            response = requests.get(url)
 
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text, 'html.parser')
-            # Find data by tag and class
-            table_element = soup.find('table')
-            if table_element:
-                html_string = StringIO(str(table_element))
-                table = pd.read_html(html_string)
-                table = table[0]
-                print(table.to_csv(f"data/{year}_fantasy.csv"))
-            else:
-                print(f"No table found for year {year}")
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.text, 'html.parser')
+                # Find data by tag and class
+                table_element = soup.find('table')
+                if table_element:
+                    html_string = StringIO(str(table_element))
+                    table = pd.read_html(html_string)
+                    table = table[0]
+                    print(table.to_csv(f"data/{year}_{week}_fantasy.csv"))
+                else:
+                    print(f"No table found for year {year}")
             
 
 if __name__ == "__main__":
